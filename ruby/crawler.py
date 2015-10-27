6c7086ccc3a7c390647d6dc78fd2825b51de8416
Nishanth Vijayan <nishanththegr8@gmail.com>
2015-10-28 02:00:29 +0530

    Crawl qx and add logging & error handling.

--- ruby/crawler.py
+++ ruby/crawler.py
@@ -1,25 +1,37 @@
 from urllib2 import urlopen
 import json
 from threading import  Thread
+from time import time
 
 def crawl(input_char):
     
     f = open("data/input/"+input_char+"_input",'r')
-    h = open("data/output/"+input_char+"_output",'w')
+    h = open("data/output/"+input_char+"_output",'a')
+    log = open("log",'a')
+
+    time1 = time()
+    counter = 0
 
     for line in f.readlines():
         gem_name = line.strip("\n")
-        page = urlopen("https://rubygems.org/api/v1/gems/"+gem_name+".json")
-
-        req = json.load(page)["dependencies"]["runtime"]
         req_list = gem_name
-        for item in req: req_list += " "+(item["name"])
-        h.write(req_list+"\n")
-
+        counter += 1
+        try:
+            page = urlopen("https://rubygems.org/api/v1/gems/"+gem_name+".json")
+            req = json.load(page)["dependencies"]["runtime"]
+            for item in req: req_list += " "+(item["name"])
+            h.write(req_list+"\n")
+        except Exception,err:
+            h.write(req_list+" "+str(err)+"\n")
     h.close()
     f.close()
 
-seq = list("yz")
+    time2 = time()
+    total_time = time2- time1
+    log.write(input_char+" "+str(int(total_time))+" "+str(counter)+'\n')
+    log.close()
+
+seq = list("wvuon")
 for letter in seq:
     t = Thread(target = crawl,args=(letter,))                                                                                                           
     t.start()
