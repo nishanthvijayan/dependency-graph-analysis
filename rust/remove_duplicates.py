8207bf95c354326003e34c3919b37384c19bc071
Nishanth Vijayan <nishanththegr8@gmail.com>
2015-11-01 18:56:30 +0530

    Fix all errors.Perfect cleaning.

--- rust/remove_duplicates.py
+++ rust/remove_duplicates.py
@@ -1,6 +1,6 @@
 f = open("edge_list",'r')
 h = open("edge_list_cleaned",'w')
 for line in f.readlines():
-    h.write(" ".join((list(set( line.split() ) ) )) + "\n")
+    h.write(line.split()[0]+" "+" ".join(list(set( line.split()[1:] ) )) + '\n')
 f.close()
 h.close()
